4af108cd416adfff5e87fde7be87481eb9b6268e
Olzhas Yerezhepov <bubbles.gums69@gmail.com>
2023-09-27 09:08:20 +0500

    Убрал лишнее, добавил комментарий

--- cypress/e2e/todo-list.spec .cy.js
+++ cypress/e2e/todo-list.spec .cy.js
@@ -1,44 +1,44 @@
 const elementCount = 3;
 const elementArray = [];
 
-describe('Проверка Todo-List', () => {
-  context('math', () => {
-    it('Перейти на сайт https://forhemer.github.io/React-Todo-List/', () => {
-      cy.visit('https://forhemer.github.io/React-Todo-List/');
+describe('Тестовое задание -- Ережепов О.', () => {
+    it('Проверка E2E Todo-List', () => {
+        
+        cy.visit('https://forhemer.github.io/React-Todo-List/');
 
-      for(let i = 0; i < elementCount; i++)
-      {
-        let elementText = `${i + 1}-й элемент`;
-        elementArray.push(elementText);
+        for(let i = 0; i < elementCount; i++)
+        {
+          let elementText = `${i + 1}-й элемент`;
+          elementArray.push(elementText);
 
-        cy.get('.input-text').click()
-        .type(elementText);
-      
-        cy.contains('Submit').click();
-      }
+          cy.get('.input-text').click()
+          .type(elementText);
+        
+          cy.contains('Submit').click();
+        }
+        
+        // далее используются хрупкие селекторы, т.к. отсутствуют data-атрибуты
+        cy.get('.inner')
+          .find('li')
+          .should('have.length', elementCount);
+        
+        cy.get('.inner')
+          .contains(elementArray[0])
+          .as('elementChangedContent')
+          .parent('li')
+          .as('elementChanged')
+          .find('input').click();
+        
+        cy.get('@elementChanged')
+          .find('span')
+          .invoke('css', 'text-decoration')
+          .should('include', 'line-through');
 
-      cy.get('.inner')
-        .find('li')
-        .should('have.length', elementCount);
-      
-      cy.get('.inner')
-        .contains(elementArray[0])
-        .as('elementChangedContent')
-        .parent('li')
-        .as('elementChanged')
-        .find('input').click();
-      
-      cy.get('@elementChanged')
-        .find('span')
-        .invoke('css', 'text-decoration')
-        .should('include', 'line-through');
+        cy.get('@elementChanged')
+          .contains('Delete')
+          .click();
 
-      cy.get('@elementChanged')
-        .contains('Delete')
-        .click();
-
-      cy.get('@elementChangedContent')
-        .should('not.exist');
-    });
-  })
-})
+        cy.get('@elementChangedContent')
+          .should('not.exist');
+      });
+});
